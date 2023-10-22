0434d7a6a1d6327176dcc9ffa88f3b96e7db7221
Romain <profurax@gmail.com>
2023-10-22 16:59:01 +0200

    fix bug console.log

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Spline from "@splinetool/react-spline";
 import Navbar from "../components/Navbar";
 import Load from "../components/Load";
-
+// 1500
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -53,30 +53,33 @@ const Home = () => {
             <path
               d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
               stroke="#ffffffff"
-              stroke-width="1"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <path
               d="M12 6V14"
               stroke="#ffffffff"
-              stroke-width="1"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
             <path
               d="M15 11L12 14L9 11"
               stroke="#ffffffff"
-              stroke-width="1.25"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.25"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </div>
       </section>
       <section className="about">
         <div className="container">
-          <Spline className="spline" scene="https://prod.spline.design/TspWfyl-7Z0Aeb0U/scene.splinecode" />
+          <Spline
+            className="spline"
+            scene="https://prod.spline.design/TspWfyl-7Z0Aeb0U/scene.splinecode"
+          />
           <div className="text">
             <h2>A propos de moi</h2>
             <p>
@@ -112,9 +115,9 @@ const Home = () => {
                 width="1998"
                 height="579"
                 filterUnits="userSpaceOnUse"
-                color-interpolation-filters="sRGB"
+                colorInterpolationFilters="sRGB"
               >
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feFlood floodOpacity="0" result="BackgroundImageFix" />
                 <feColorMatrix
                   in="SourceAlpha"
                   type="matrix"
